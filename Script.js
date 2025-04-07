4e94a96864f536ed51107f3065021ba6462f1a99
Ahmed-Hersh <ahmed.geo95@gmail.com>
2025-04-08 02:30:20 +0300

    Create Script.js

---
+++ Script.js
@@ -0,0 +1,4 @@
+// يمكنك إضافة تفاعلات مثل قائمة الهاتف المتحرك
+document.querySelector('.hamburger').addEventListener('click', function() {
+    document.querySelector('.nav-links').classList.toggle('active');
+});
