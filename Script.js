69c98610bbdbba0115d61c19cfe0f61d255a63b0
Tasty FastFood <ahmed.geo95@gmail.com>
2025-04-14 19:38:19 +0300

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,4 +1,1 @@
-// هذا هو الكود الذي تريد استخدامه
-document.querySelector('.hamburger').addEventListener('click', function() {
-    document.querySelector('.nav-links').classList.toggle('active');
-});
+
